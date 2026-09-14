f78322f4f28fb4c6d48c7fd268e47c2e07c722a0
Guy Diskin <guydiskin@gmail.com>
2026-09-14 17:11:57 +0300

    networking 1

---
+++ server.py
@@ -0,0 +1,44 @@
+import argparse
+import sys
+import socket
+import struct
+
+
+def run_server(ip, port):
+    ''' Recieves an IP address and a port and creates a socket to listen and
+    print the data sent to said address'''
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((ip, port))
+        while True:
+            s.listen(5)
+            conn, addr = s.accept()
+            text = conn.recv(1024)
+            data = struct.unpack(f"<{len(text)}s", text)
+            data = data[0].decode()
+            print(f"Recieved data: {data}")
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Send data to server.')
+    parser.add_argument('server_ip', type=str,
+                        help="the server's ip")
+    parser.add_argument('server_port', type=int,
+                        help="the server's port")
+    return parser.parse_args()
+
+
+def main():
+    '''
+    Implementation of CLI and sending data to server.
+    '''
+    args = get_args()
+    try:
+        run_server(args.server_ip, args.server_port)
+        print('Done.')
+    except Exception as error:
+        print(f'ERROR: {error}')
+        return 1
+
+
+if __name__ == '__main__':
+    sys.exit(main())
